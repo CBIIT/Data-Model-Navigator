11735988bdbbe6ba92bff8916332693ff498af88
rana22 <ambar.rana123@gmail.com>
2022-11-10 14:45:18 -0500

    Legend Component code refactor

--- src/components/DataDictionaryComponent/DataDictionary/graph/Legend/LegendView.jsx
+++ src/components/DataDictionaryComponent/DataDictionary/graph/Legend/LegendView.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { Button, withStyles } from '@material-ui/core';
-import { capitalizeFirstLetter } from '../../../utils';
+import { withStyles } from '@material-ui/core';
 import { getLegendCategoryIconSVG, getCategoryColor } from '../../NodeCategories/helper';
 import relationshipSvg from '../../NodeCategories/icons/Legend/lg_relationship_links.svg';
 import toggleSvg from '../../NodeCategories/icons/Legend/lg_link.svg';
 import Styles from './LegendStyle';
+import { capitalizeFirstLetter } from '../../../utils';
 
 const Legend = ({
   classes,
@@ -14,29 +14,28 @@ const Legend = ({
   const toggleLegend = () => setDisplay(!display);
 
   const categoryListComponent = categoryItems.map((category) => {
-    const categoryColor = getCategoryColor(category);
+    // const categoryColor = getCategoryColor(category);
     const IconSvg = getLegendCategoryIconSVG(category);
-    console.log(category);
     return (
       <div
         key={category}
         className={classes.category}
       >
-        <span className={classes.circleWrapper}>
-          { IconSvg ? <IconSvg/>: (
-            <span className={classes.circleWrapper} style={{ backgroundColor: categoryColor }} />
-            )
-          }
-        </span>
+        <div className={classes.categoryIcon}>
+            <IconSvg/>
+        </div>
         <span className={classes.text}>{capitalizeFirstLetter(category)}</span>
       </div>
     )         
   });
 
   const ToggleBtn = () => (
-    <>
+    <div className={display ? classes.headerExpand : classes.headerCollapse}>
+      {display && (
+        <span className={classes.headerTitle}>Node Category</span>
+      )}
       <span
-        className={classes.info}
+        className={classes.toggleBtn}
         onClick={toggleLegend}
         onKeyPress={toggleLegend}
         role='button'
@@ -44,28 +43,27 @@ const Legend = ({
       >
         <img src={toggleSvg} alt="toggle Legend" />
       </span>
-    </>
+    </div>
   );
 
   return (
     <>
-     <div className={classes.legend}>
+     <div className={display ? classes.legendExapand : classes.legendCollapse}>
       {
-        display ? (
         <>
           <ToggleBtn />
-          <div className={classes.item}>
-            <img src={relationshipSvg} alt="relation" />
-            <span className={classes.text}>Relationships</span>
-          </div>
-          {categoryListComponent}
-        </>)
-        
-        : (
-          <>
-            <ToggleBtn />
-          </>
-        )
+          {
+            display && (
+              <>
+                <div className={classes.item}>
+                  <img src={relationshipSvg} alt="relation" />
+                  <span className={classes.text}>relationship links</span>
+                </div>
+                {categoryListComponent}
+              </>
+            )
+          }
+        </>
       }
      </div>
     </>
