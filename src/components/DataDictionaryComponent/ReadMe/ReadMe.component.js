79e8ac72ee4069557365a5f68e3165559ba86d52
rana22 <ambar.rana123@gmail.com>
2022-11-18 15:44:47 -0500

    create html tag for pdf

--- src/components/DataDictionaryComponent/ReadMe/ReadMe.component.js
+++ src/components/DataDictionaryComponent/ReadMe/ReadMe.component.js
@@ -9,24 +9,38 @@ import {
 } from '@material-ui/core';
 import { saveAs } from 'file-saver';
 import { ArrowDownward } from '@material-ui/icons';
+// import MarkdownPDF from "markdown-pdf";
 import CloseIcon from '@material-ui/icons/Close';
 import { pdf } from '@react-pdf/renderer';
 import ReactMarkdown from 'react-markdown';
+import { marked } from "marked";
 import PdfTemplate from './ReadMePdf';
 import styles from './ReadMe.style';
 import CustomTheme from './ReadMe.theme.config';
 import { createFileName } from '../utils';
 
 export const downloadFile = async (title, content) => {
-  const blob = await pdf((
-    <PdfTemplate
-      title={title}
-      content={content}
-    />
-  )).toBlob();
-  const fileName = createFileName('read_me', 'ICDC_Data_Model-');
-  saveAs(blob, `${fileName}.pdf`);
+  const cossnt = await marked(content);
+  console.log(cossnt);
+  // const blob = await pdf((
+  //   <PdfTemplate
+  //     title={title}
+  //     content={content}
+  //   />
+  // )).toBlob();
+  // const fileName = createFileName('read_me', 'ICDC_Data_Model-');
+  // saveAs(blob, `${fileName}.pdf`);
 };
+
+const downloadMarkdownPdf = (title, content) => {
+  // const MdPdf = markDwnContent(content);
+  // downloadFile(title, content)
+  // console.log(cont);
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = marked(content);
+  console.log(wrapper);
+}
+
 
 const ReadMeDialogComponent = ({
   classes,
@@ -34,7 +48,13 @@ const ReadMeDialogComponent = ({
   displayReadMeDialog,
   content,
   title,
-}) => (
+}) => {
+
+  if(!content) {
+    return (<></>)
+  }
+
+  return (
   <CustomTheme>
     <Dialog
       open={display}
@@ -56,7 +76,7 @@ const ReadMeDialogComponent = ({
           <Button
             className={classes.downloadBtn}
             startIcon={<ArrowDownward className={classes.downloadIcon} id="download_arrow_all" />}
-            onClick={() => downloadFile(title, content)}
+            onClick={() => downloadMarkdownPdf(title, content)}
           />
           <IconButton
             onClick={displayReadMeDialog}
@@ -68,13 +88,16 @@ const ReadMeDialogComponent = ({
           </IconButton>
         </div>
       </div>
-      <div className={classes.content} id="readMe_content">
+      <div
+        className={classes.content}
+        id="readMe_content"
+      >
         <ReactMarkdown>
           {content}
         </ReactMarkdown>
       </div>
     </Dialog>
   </CustomTheme>
-);
+)};
 
 export default withStyles(styles)(ReadMeDialogComponent);
