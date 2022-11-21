8a36ae2668dbc61b53c7ca36d006ee5630656746
rana22 <ambar.rana123@gmail.com>
2022-11-21 11:51:35 -0500

    set pdf footer

--- src/components/DataDictionaryComponent/ReadMe/ReadMe.component.js
+++ src/components/DataDictionaryComponent/ReadMe/ReadMe.component.js
@@ -22,49 +22,72 @@ import { createFileName } from '../utils';
 
 export const downloadFile = async (title, content) => {
   const cossnt = await marked(content);
-  console.log(cossnt);
-  // const blob = await pdf((
-  //   <PdfTemplate
-  //     title={title}
-  //     content={content}
-  //   />
-  // )).toBlob();
-  // const fileName = createFileName('read_me', 'ICDC_Data_Model-');
-  // saveAs(blob, `${fileName}.pdf`);
-};
-
-const downloadMarkdownPdf = async (title, content) => {
-  // const MdPdf = markDwnContent(content);
-  // downloadFile(title, content)
-  // console.log(cont);
-  const wrapper = document.createElement('div');
-  wrapper.innerHTML = marked(content);
-  
-  console.log(wrapper);
-  const page = document.getElementById('readMe_content');
-  // page.innerHTML = marked(content);
-  console.log(page)
-  // await html2PDF(page, {
-  //   jsPDF: {
-  //     format: 'a4',
-  //   },
-  //   imageType: 'image/jpeg',
-  //   output: './pdf/generate.pdf'
-  // });
-  const element = document.getElementById('readMe_content');
-  const opt = {
-    margin:       1,
-    filename:     'myfile.pdf',
-    image:        { type: 'jpeg', quality: 0.98 },
-    html2canvas:  { scale: 2 },
-    jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    console.log(cossnt);
+    // const blob = await pdf((
+    //   <PdfTemplate
+    //     title={title}
+    //     content={content}
+    //   />
+    // )).toBlob();
+    // const fileName = createFileName('read_me', 'ICDC_Data_Model-');
+    // saveAs(blob, `${fileName}.pdf`);
   };
-  // New Promise-based usage:
-  // html2pdf().set(opt).from(element).save();
-  html2pdf().set(opt).from(wrapper).toPdf().get('pdf').then(function (pdf) {
-    pdf.addPage();
-  }).toPdf().save();
-} 
+
+  function pdfCallback(pdf) {
+    const totalPages = pdf.internal.getNumberOfPages();
+    const pageSz = pdf.internal.pageSize;
+    for (let i = 1; i <= totalPages; i++) {
+      pdf.setPage(i);
+      pdf.setFontSize(5);
+      pdf.setTextColor(0);
+      pdf.text(pageSz.getWidth() - 1, pageSz.getHeight() - 0.5, `YOUR_TEXT ${i}`);
+      pdf.text(pageSz.getWidth() - 8, pageSz.getHeight() - 0.5, `CANINECOMMONS.CANCER.GOV/#/ICDC-DATA-MODEL`);
+      pdf.addImage("YOUR_IMAGE", 'JPEG', pageSz.getWidth() - 1.1, pageSz.getHeight() - 0.25, 1, 0.2);
+    }
+}
+
+  const downloadMarkdownPdf = async (title, content) => {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML += '<span>Understanding the ICDC Data Model</span>';
+    wrapper.innerHTML += marked(content);
+    
+    console.log(wrapper);
+    const opt = {
+      margin:       1,
+      filename:     'myfile.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    // html2pdf(wrapper, {
+    //   margin: 10,
+    //   filename: "my.pdf",
+    //   image: {type: 'jpeg', quality: 1},
+    //   html2canvas: {dpi: 1000, letterRendering: true},
+    //   jsPDF: {unit: 'mm', format: 'a4', orientation: 'portrait'},
+    //   pdfCallback: pdfCallback
+    // })
+
+    html2pdf()
+      .set(opt)
+      .from(wrapper)
+      .toPdf()
+      .get('pdf')
+      .then((pdf) => {
+        pdf.addPage();
+        const totalPages = pdf.internal.getNumberOfPages();
+        const pageSz = pdf.internal.pageSize;
+        for (let i = 1; i <= totalPages; i++) {
+          pdf.setPage(i);
+          pdf.setFontSize(5);
+          pdf.setTextColor(0);
+          pdf.text(pageSz.getWidth() - 1.5, pageSz.getHeight() - 0.5, `YOUR_TEXT ${i}`);
+          pdf.text(pageSz.getWidth() - 7.5, pageSz.getHeight() - 0.5, `CANINECOMMONS.CANCER.GOV/#/ICDC-DATA-MODEL`);
+          // pdf.addImage("YOUR_IMAGE", 'JPEG', pageSz.getWidth() - 1.1, pageSz.getHeight() - 0.25, 1, 0.2);
+        }
+    }).save();
+}
 
 const ReadMeDialogComponent = ({
   classes,
