e3b11999619e3532d37afd58a37b8fe6eadca94e
rana22 <ambar.rana123@gmail.com>
2022-12-28 12:52:37 -0500

    update version

--- src/components/DataDictionaryComponent/DataDictionary/ReactFlowGraph/node/NodeStyle.jsx
+++ src/components/DataDictionaryComponent/DataDictionary/ReactFlowGraph/node/NodeStyle.jsx
@@ -2,14 +2,11 @@ export default () => ({
   propDialog: {
     backgroundColor: '#2D4455',
     borderRadius: '2px',
-    // border: '1.5px solid #0C3759',
-    // width: '200px'
   },
   customNodeCollapse: {
     fontSize: '10px',
     // background: '#f5f5f6',
     color: '#222',
-    // boxShadow: '0 4px 6px -1px rgb(0 0 0 / 15%), 0 2px 4px -1px rgb(0 0 0 / 8%)',
     borderRadius: '2px',
     padding: '10px 10px 0px 10px',
     background: 'transparent',
@@ -111,57 +108,37 @@ export default () => ({
   },
   administrative: {
     '& img': {
-      // background: '#9b2d20',
-      // borderLeft: '2px solid #9b2d20',
-      // borderRight: '2px solid #9b2d20',
-      // marginLeft: '-1px',
-      // marginTop: '-1px',
-      // borderTopLeftRadius: '2px',
-      // borderBottomLeftRadius: '2px',
-
       background: '#9b2d20',
     }
   },
   study: {
     '& img': {
       background: '#9875ff',
-      // borderLeft: '2px solid #9875ff',
-      // borderRight: '2px solid #9875ff',
     }
   },
   case: {
     '& img': {
       background: '#ff7f15',
-      // borderLeft: '2px solid #ff7f15',
-      // borderRight: '2px solid #ff7f15',
     }
   },
   clinical_trial: {
     '& img': {
       background: '#00A1BB',
-      // borderLeft: '2px solid #00A1BB',
-      // borderRight: '2px solid #00A1BB',
     }
   },
   biospecimen: {
     '& img': {
       background: '#00785a',
-      // borderLeft: '2px solid #00785a',
-      // borderRight: '2px solid #00785a',
     }
   },
   analysis: {
     '& img': {
       background: '#b533a9',
-      // borderLeft: '2px solid #b533a9',
-      // borderRight: '2px solid #b533a9',
     }
   },
   data_file: {
     '& img': {
       background: '#00ad0e',
-      // borderLeft: '2px solid #00ad0e',
-      // borderRight: '2px solid #00ad0e',
     }
   },
   clinical: {
