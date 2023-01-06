1b6bf3533058676dfebb265bf614c5488c32623c
rana22 <ambar.rana123@gmail.com>
2023-01-06 10:31:53 -0500

    move service file

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ export { default as ReduxDataDictionary } from './components/ModelNavigator/Data
 // export { default as ModelExplorer } from './components/DataDictionaryComponent/dictionaryController';
 export { ddgraph as ddgraph, versionInfo as versionInfo } from './components/ModelNavigator/DataDictionary/Store/reducers/graph';
 export { moduleReducers as moduleReducers} from './components/ModelNavigator/DataDictionary/Store/reducers/filter';
-export { getModelExploreData } from './components/ModelNavigator/DictionaryControllerUtil';
+export { getModelExploreData } from './components/ModelNavigator/DataDictionary/Service/Dictionary';
