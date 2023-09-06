21926d84764dc5b354bff608d59be029d09c609f
Udosen <udosent2@nih.gov>
2023-09-06 16:31:08 -0500

    DMDN nodeView design QA

--- src/components/ModelNavigator/DataDictionary/ReactFlowGraph/Node/NodeView.jsx
+++ src/components/ModelNavigator/DataDictionary/ReactFlowGraph/Node/NodeView.jsx
@@ -1,10 +1,10 @@
-import React, { memo, useState, useEffect } from 'react';
-import { withStyles } from '@material-ui/core';
-import CloseIcon from '@material-ui/icons/Close';
-import { Handle, useReactFlow, useStoreApi } from 'reactflow';
-import clsx from 'clsx';
-import Styles from './NodeStyle';
-import { highlightMatchingTitle, setMatchingNodeClasses } from './util';
+import React, { memo, useState, useEffect } from "react";
+import { withStyles } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import { Handle, useReactFlow, useStoreApi } from "reactflow";
+import clsx from "clsx";
+import Styles from "./NodeStyle";
+import { highlightMatchingTitle, setMatchingNodeClasses } from "./util";
 
 const NodeView = ({
   classes,
@@ -20,21 +20,21 @@ const NodeView = ({
   onCollapseNodeView,
   highlightingNode,
   onNodeFocus,
-  focusedNodeId
+  focusedNodeId,
 }) => {
   const [display, setDisplay] = useState(false);
   /**
    * expand node in normal mode (when search mode is false)
-   * use view option to adjust the fontSize on property dialog 
+   * use view option to adjust the fontSize on property dialog
    */
   const expandNode = () => {
-    const view = localStorage.getItem('reactflowGraphView');
+    const view = localStorage.getItem("reactflowGraphView");
     onClickNode(id);
     setDisplay(!display);
-    if (display) { 
+    if (display) {
       onCollapseNodeView();
     }
-  }
+  };
   const {
     label,
     icon,
@@ -51,7 +51,7 @@ const NodeView = ({
   const displayOverviewTable = () => {
     onClickNode(id);
     onViewTable(false);
-  }
+  };
 
   /**
    * light node based on reasult of search query
@@ -60,8 +60,7 @@ const NodeView = ({
     if (!expandNodeView) {
       setDisplay(false);
     } else {
-      if (`${label}`.toLowerCase() === highlightingNode?.id
-      ) {
+      if (`${label}`.toLowerCase() === highlightingNode?.id) {
         setDisplay(true);
       } else {
         setDisplay(false);
@@ -88,109 +87,114 @@ const NodeView = ({
 
   return (
     <>
-      <div className={clsx({[classes.propDialog]: display})}>
-        <div className={display ? classes.customNodeExpand : classes.customNodeCollapse}>
+      <div className={clsx({ [classes.propDialog]: display })}>
+        <div
+          className={
+            display ? classes.customNodeExpand : classes.customNodeCollapse
+          }
+        >
           {display && (
             <div className={classes.iconBar}>
               <CloseIcon className={classes.closeIcon} onClick={expandNode} />
             </div>
           )}
-          <div className={clsx(classes.nodeTitle, {[classes.btnPadding]: display})}>
-            <button className={isSearchMode ? nodeClasses
-              : clsx(classes.nodeTitleBtn,
-                  classes.categoryIcon,
-                  classes[category],
-                  {[classes.nodeTitleBtnWrapper]: display})}
+          <div
+            className={clsx(classes.nodeTitle, {
+              [classes.btnPadding]: display,
+            })}
+          >
+            <button
+              className={
+                isSearchMode
+                  ? nodeClasses
+                  : clsx(
+                      classes.nodeTitleBtn,
+                      classes.categoryIcon,
+                      classes[category],
+                      { [classes.nodeTitleBtnWrapper]: display }
+                    )
+              }
               onClick={isSearchMode ? displayOverviewTable : expandNode}
               onFocus={nodeFocusEvent}
-            > 
-              <span className={classes.iconWrapper} 
-                style={{backgroundImage: `url(${icon})`, 
-                backgroundRepeat: 'no-repeat'}}
+            >
+              <span
+                className={classes.iconWrapper}
+                style={{
+                  backgroundImage: `url(${icon})`,
+                  backgroundRepeat: "no-repeat",
+                }}
               >
                 {/* <img className={classes.nodeIcon} src={icon} alt="category_icon" />  */}
               </span>
               <p className={classes.nodeName}>
-                {(isSearchMode && matchedNodeNameQuery) ?
-                  (<>
-                    {highlightMatchingTitle(label, matchedNodeNameQuery, classes)}
-                  </>) : `${label}`.toLowerCase()}
+                {isSearchMode && matchedNodeNameQuery ? (
+                  <>
+                    {highlightMatchingTitle(
+                      label,
+                      matchedNodeNameQuery,
+                      classes
+                    )}
+                  </>
+                ) : (
+                  `${label}`.toLowerCase()
+                )}
               </p>
             </button>
           </div>
           <div className={display ? classes.viewSection : classes.hideSection}>
             <ul className={classes.list}>
               <li className={classes.listItem}>
-                <div className={classes.content}>
-                  <span className={classes.listItemLabel}>
-                    {'Assignment: '}
-                  </span>
-                  <span className={classes.listItemValue}>
-                    {nodeAssignment}
-                  </span>
-                </div>
+                <span className={classes.listItemLabel}>{"Assignment:"}</span>
+                <span className={classes.listItemValue}>{nodeAssignment}</span>
               </li>
-              <hr className={classes.divider}/>
+              <hr className={classes.divider} />
               <li className={classes.listItem}>
-                <div className={classes.content}>
-                  <span className={classes.listItemLabel}>
-                    {'Class: '}
-                  </span>
-                  <span className={classes.listItemValue}>
-                    {nodeClass}
-                  </span>
-                </div>
+                <span className={classes.listItemLabel}>{"Class: "}</span>
+                <span className={classes.listItemValue}>{nodeClass}</span>
               </li>
-              <hr className={classes.divider}/>
+              <hr className={classes.divider} />
               <li className={classes.listItem}>
-                <div className={classes.content}>
-                  <span className={classes.listItemLabel}>
-                    {'Required Properties: '}
-                  </span>
-                  <span className={classes.listItemValue}>
-                    {reqPropsCount}
-                  </span>
-                </div>
+                <span className={classes.listItemLabel}>
+                  {"Required Properties: "}
+                </span>
+                <span className={classes.listItemValue}>{reqPropsCount}</span>
               </li>
-              <hr className={classes.divider}/>
+              <hr className={classes.divider} />
               <li className={classes.listItem}>
-                <div className={classes.content}>
-                  <span className={classes.listItemLabel}>
-                    {'Preferred Properties: '}
-                  </span>
-                  <span className={classes.listItemValue}>
-                    {prefPropsCount}
-                  </span>
-                </div>
+                <span className={classes.listItemLabel}>
+                  {"Preferred Properties: "}
+                </span>
+                <span className={classes.listItemValue}>{prefPropsCount}</span>
               </li>
-              <hr className={classes.divider}/>
+              <hr className={classes.divider} />
               <li className={classes.listItem}>
-                <div className={classes.content}>
-                  <span className={classes.listItemLabel}>
-                    {'Optional Properties: '}
-                  </span>
-                  <span className={classes.listItemValue}>
-                    {optPropsCount}
-                  </span>
-                </div>
+                <span className={classes.listItemLabel}>
+                  {"Optional Properties: "}
+                </span>
+                <span className={classes.listItemValue}>{optPropsCount}</span>
               </li>
             </ul>
           </div>
-          <Handle type="target" position="top" style={{ top: '12px' }} />
+          <Handle type="target" position="top" style={{ top: "12px" }} />
           <Handle
             type="source"
             position="bottom"
             id={handleId}
             style={{
-              background: 'transparent',
-              border: 'none',
-              top: '37px',
+              background: "transparent",
+              border: "none",
+              top: "37px",
             }}
           />
         </div>
-        {display && 
-          <button className={classes.viewPropBtn} onClick={displayOverviewTable}>View Properties</button>
-        }
+        {display && (
+          <button
+            className={classes.viewPropBtn}
+            onClick={displayOverviewTable}
+          >
+            View Properties
+          </button>
+        )}
       </div>
     </>
   );
