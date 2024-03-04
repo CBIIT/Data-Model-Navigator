72d2b5fbbe3fa8dd1cf79bc706088989f2b2be60
Alec M <alec.mattu@nih.gov>
2024-03-04 10:31:27 -0500

    fix: Remove ICDC-specific props from File Trans. Manifest

--- src/config/file-manifest-config.js
+++ src/config/file-manifest-config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const fileManifestDownload = [
-  ['original_file_size', { src: 'Config-derived' }],
-  ['original_md5sum', { src: 'Config-derived' }],
+  // ['original_file_size', { src: 'Config-derived' }],
+  // ['original_md5sum', { src: 'Config-derived' }],
 ];
 
 export const fileManifestDownloadSettings = {
