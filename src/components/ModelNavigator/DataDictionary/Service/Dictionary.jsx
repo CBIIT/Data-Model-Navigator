8facef2bb883f5b9ae3232cf2b46f447a99a9a45
Toyo <toyo.udosen@nih.gov>
2024-09-04 11:40:26 -0500

    refactor import

--- src/components/ModelNavigator/DataDictionary/Service/Dictionary.jsx
+++ src/components/ModelNavigator/DataDictionary/Service/Dictionary.jsx
@@ -9,7 +9,7 @@
 // import React from 'react';
 import axios from 'axios';
 import yaml from 'js-yaml';
-import _, { noop } from 'lodash';
+import { startCase } from 'lodash';
 
 const version = { commit: '913161064b02bcef024d072873e77c8c79cc1a68', dictionary: { commit: '520a25999fd183f6c5b7ddef2980f3e839517da5', version: '0.2.1-9-g520a259' }, version: '4.0.0-44-g9131610' };
 const DATA_MODEL = 'https://raw.githubusercontent.com/CBIIT/icdc-model-tool/develop/model-desc/icdc-model.yml';
@@ -155,7 +155,7 @@ async function getModelExploreData(modelUrl = DATA_MODEL, modelPropsUrl = DATA_M
     }
 
     for (const property in icdcMData.Relationships) {
-      item.multiplicity = _.startCase(icdcMData.Relationships[property].Mul);
+      item.multiplicity = startCase(icdcMData.Relationships[property].Mul);
       const label = propertyName;
       // const multiplicity = icdcMData.Relationships[propertyName].Mul;
       const required = false;
