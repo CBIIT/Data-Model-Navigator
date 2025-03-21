a9e8cbd2d6fbd9ad970a5ee0d8d39ec824c1fc76
Alec M <alec.mattu@nih.gov>
2025-03-21 09:38:44 -0400

    fix: Link should target a new tab

--- src/components/ModelNavigator/DataDictionary/ReadMe/ReadMe.component.js
+++ src/components/ModelNavigator/DataDictionary/ReadMe/ReadMe.component.js
@@ -191,7 +191,7 @@ const ReadMeDialogComponent = ({
           </div>
         </div>
         <div className={classes.content} id="readMe_content">
-          <ReactMarkdown>
+          <ReactMarkdown linkTarget="_blank">
             {content.replace(/<!-- PAGE BREAK -->/g, "")}
           </ReactMarkdown>
         </div>
