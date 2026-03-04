19317691cebedab91857845177a2dfc75c82bbf3
nathanhuangnih <nathaniel.huang@nih.gov>
2026-03-04 16:48:14 -0500

    CTDC-1714: node modals closing function (#171)
    * node modals closing function

--- src/components/ModelNavigator/DataDictionary/ReactFlowGraph/Node/NodeView.jsx
+++ src/components/ModelNavigator/DataDictionary/ReactFlowGraph/Node/NodeView.jsx
@@ -88,9 +88,10 @@ const NodeView = ({
     onNodeFocus(id);
   };
 
+
   return (
     <>
-      <div className={clsx({ [classes.propDialog]: display })}>
+      <div className={clsx({ [classes.propDialog]: display })} >
         <div
           className={
             display ? classes.customNodeExpand : classes.customNodeCollapse
@@ -114,9 +115,12 @@ const NodeView = ({
                 style={{
                   border: display && "2px solid white",
                 }}
-                onClick={isSearchMode ? displayOverviewTable : expandNode}
+                onClick={isSearchMode ? displayOverviewTable : display ? undefined : expandNode}
               >
-                <div className={classes.nodeButtonInnerWrapper}>
+                <div className={clsx(
+                  classes.nodeButtonInnerWrapper,
+                  { [classes.nodeButtonInnerWrapperOnExpand]: expandNodeView }
+                  )}>
                   <div
                     style={{
                       borderRadius: "11px",
@@ -139,7 +143,10 @@ const NodeView = ({
                     </div>
                   </div>
 
-                  <div className={classes.labelWrapper}>
+                  <div className={clsx(
+                    display && !isSearchMode ? classes.labelWrapperExpanded : classes.labelWrapper,
+                    { [classes.labelWrapperCollapse]: !expandNodeView },
+                    )}>
                     {isSearchMode && matchedNodeNameQuery ? (
                       <>
                         {highlightMatchingTitle(
